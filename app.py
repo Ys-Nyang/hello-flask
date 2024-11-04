f1c446ba8b1a4ecc1584d942ca5f7d13683ef079
허태웅 <did20031@naver.com>
2024-11-04 12:17:17 +0900

    입력한 이름으로 유튜브 검색 따라하기

--- app.py
+++ app.py
@@ -9,9 +9,23 @@ def hello():
 def naver():
     return render_template('naver.html')
 
-@app.route('/youtube')
+@app.route('/ytpage')
+def ytpage():
+    return render_template('ytpage.html')
+
+@app.route('/youtube', methods=['GET', 'POST'])
 def youtube():
-    return render_template('youtube.html')
+    linklist = ['https://www.youtube.com/embed/Ox_wfL_JJy0?si=J5l2cNRpflgRk-l_', 'https://www.youtube.com/embed/ax1csKKQnns?si=7D1hYXwDIHqZt8nY']
+    keyword = request.form["keyword"]
+    print(keyword)
+    linknum = 0 # 0은 윤하, 1은 아이유
+    if keyword == '윤하':
+        linknum = 0
+    elif keyword == '아이유':
+        linknum = 1
+    else:
+        print('잘못된 입력입니다. 올바른 키워드를 입력하세요')
+    return render_template('youtube.html', name=keyword, link=linklist[linknum])
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
