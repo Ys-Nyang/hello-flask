0d311081f208e6f37ae9931f872c3ae041f3812a
허태웅 <did20031@naver.com>
2024-11-04 10:47:10 +0900

    페이지 추가

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello():
 def naver():
     return render_template('naver.html')
 
+@app.route('/youtube')
+def youtube():
+    return render_template('youtube.html')
+
 @app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
