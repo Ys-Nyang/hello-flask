231ea9eb837db8a43df6de5e013756bb3adcaac3
허태웅 <did20031@naver.com>
2024-11-18 12:06:36 +0900

    네이버 api 검색어 맛집 링크까지

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, render_template
+import nvapi
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/baseball')
+def baseball():
+    return 'Hello, baseball!'
 
 @app.route('/naver')
 def naver():
@@ -34,7 +40,9 @@ def method():
     else:
         keyword = request.form["keyword"]
         print(keyword)
-        return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        data = nvapi.blog(keyword)
+        # return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        return render_template("result.html", keyword=keyword, blist=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
